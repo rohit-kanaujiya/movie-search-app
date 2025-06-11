5086732a37ddc610f1eef535a89cb6fec5363603
Rohit kanaujiya <rohitkanaujiya0012@gmail.com>
2025-06-12 01:06:34 +0530

    Fix api.js to correctly use environment variables and axios params

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 const BASE_URL = 'https://www.omdbapi.com/';
-
 export const searchMovies = async (query, page = 1) => {
   const response = await axios.get(BASE_URL, {
     params: {
